588fc6927e047b5b3da5aee59b1cda9e90c846cb
TheoMLP <theomalaper.cognez@gmail.com>
2019-07-24 11:27:52 +0000

    first-file-module-4

---
+++ data-types-variables-operators.js
@@ -0,0 +1,60 @@
+//Arithmetic operators 
++ - * / ** %
+
+//Comparison operators 
+< less than
+<= less than or equal to
+> greater than
+>= greater than or equal to
+=== strict equals—that is, the two values have to be of the same type and have the same value 
+!== strict inequality—that is two values being compared are not of the same type and
+ do not have the same value.
+
+//boolean value 
+true false
+
+//comments 
+single line // multiple line /* */ 
+
+//access letter in string
+var name = "theo"
+console.log(name[3])
+
+//escaping string, backslash to use quote inside of a string
+'I/'m good'
+"The man whispere \"please help \" "
+\(backslash)
+\"
+\'
+\n : newline 
+\t : tab
+
+//null
+data types that has no value on purpose 
+var x = null
+
+//NaN
+returns not a number 
+console.log("hello" / 5)
+NaN
+
+//undefined
+data type that indicates the absence of value
+var x 
+console.log(x)
+undefined
+
+//number coercion 
+use === !== instead of == != otherwise java will try 
+to convert both value into the same type of date
+
+//put semicolons at the end of lines of code 
+makes the code clearner and with less bugs
+
+//let and var 
+creates a variable assign it a value and allows us to reassign 
+it a value later 
+
+//const 
+with const the assigned value will never change the variable
+cannot be reassigned 
---
+++ tinkering.js
@@ -0,0 +1,6 @@
+function multiply(a, b) {
+  return a * b 
+}
+
+const result = multiply(4, 4)
+console.log(result)
