57dc2d382c8007c14aabe38ee6bc2e0a5404f856
TheoMLP <theomalaper.cognez@gmail.com>
2019-07-29 08:29:18 +0000

    yes

--- packing-list.js
+++ packing-list.js
@@ -1,6 +1,13 @@
 const packingList = ["bowls", "plates", "pots", "pans", "eating utensils", "glasses", "cups", "cooking utensils"];
 
 console.log("Kitchen stuff to pack:")
+
 for (var i = 0; i < packingList.length; i += 1) {
   console.log(packingList[i])
+}
+
+var i = 0 
+while(i < packingList.length) {
+  console.log(packingList[i]);
+  i++;
 }
